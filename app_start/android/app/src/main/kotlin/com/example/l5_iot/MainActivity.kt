94c9cc4af6bbf79db63d0517de41df39890163e4
Xomerian <77843593+Xomerian@users.noreply.github.com>
2022-05-20 16:45:19 +0300

    Lab 6

--- app_start/android/app/src/main/kotlin/com/example/l5_iot/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.l5_iot
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- app_start/lib/product.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Product {
-  final String name;
-
-  const Product({@required this.name});
-}
