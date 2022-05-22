3c394fe91bbf99dcbc67c8551e1230a838f207e3
Xomerian <77843593+Xomerian@users.noreply.github.com>
2022-05-22 16:47:24 +0300

    Lab 5 - fav cart

--- iot_l6/lib/main.dart
+++ iot_l6/lib/main.dart
@@ -11,6 +11,9 @@ import 'package:flutter_signin_button/button_builder.dart';
 
 import './register_page.dart';
 import './signin_page.dart';
+import './ProductPage.dart';
+import './product.dart';
+import './FavoritePage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,26 +33,52 @@ class AuthExampleApp extends StatelessWidget {
       title: 'Firebase Example App',
       theme: ThemeData.dark(),
       home: Scaffold(
-        body: AuthTypeSelector(),
+        body: AuthTypeSelector([],[]),
       ),
     );
   }
 }
 
+class AuthTypeSelector extends StatefulWidget {
+  AuthTypeSelector(this.shoppingCart, this.favourites) : super();
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final List<Product> shoppingCart;
+  final List<Product> favourites;
+
+  @override
+  _AuthTypeSelectorState createState() => _AuthTypeSelectorState();
+}
+
 /// Provides a UI to select a authentication type page
-class AuthTypeSelector extends StatelessWidget {
+class _AuthTypeSelectorState extends State<AuthTypeSelector> {
+  int _selectedPage = 0;
   // Navigates to a new page
   void _pushPage(BuildContext context, Widget page) {
     Navigator.of(context) /*!*/ .push(
       MaterialPageRoute<void>(builder: (_) => page),
     );
   }
-
+  Future<void> NavBar(BuildContext context) async {
+    if (await FirebaseAuth.instance.currentUser != null) {
+      Text('working');
+    } else {
+      Text('working2');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Firebase Example App'),
+        title: const Text('Home'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -76,6 +105,48 @@ class AuthTypeSelector extends StatelessWidget {
           ),
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home,color : Colors.black),
+            label: 'HomePage',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list,color : Colors.black),
+            label: 'Product',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite, color : Colors.black),
+            label: 'Favorite',
+          ),
+        ],
+        currentIndex: _selectedPage,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedPage = index;
+    });
+    if(index==0) _navigateToHomePage(context);
+    else if(index==1) _navigateToProductPage(context);
+    else _navigateToFavoritePage(context);
+  }
+  void _navigateToHomePage(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => AuthTypeSelector(widget.shoppingCart,widget.favourites)));
+  }
+  void _navigateToProductPage(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => ProductPage(widget.shoppingCart,widget.favourites)));
+  }
+  void _navigateToFavoritePage(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => FavoritePage(widget.shoppingCart,widget.favourites)));
+  }
 }
+
+
+
